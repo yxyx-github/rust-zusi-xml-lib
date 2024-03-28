42241400c1ad4865993ab2c0f17479f31d73fae6
yxyx-github <dev_mw@vodafonemail.de>
2024-03-28 10:19:40 +0100

    add some documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,7 @@
+//! Library to serialize/deserialize xml files for [Zusi 3](https://www.zusi.de/) using Serde.
+//!
+//! It provides the necessary data structures for usage with Serde and a TryFrom implementation.
+
+/// Includes all the required data structures. At the moment `.result.xml` files are supported only.
+/// For details about the schema, please refer to the demo files included in the [Zusi](https://www.zusi.de/) installation.
 pub mod xml;
