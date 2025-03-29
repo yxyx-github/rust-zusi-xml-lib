1ec69778f13517cc73c41a15ad5ced719ca6423b
yxyx-github <dev_mw@vodafonemail.de>
2025-03-29 17:37:25 +0100

    increase precision for DelphiTimestamp conversions

--- src/delphi_timestamp.rs
+++ src/delphi_timestamp.rs
@@ -2,7 +2,7 @@ use time::macros::datetime;
 use time::{Duration, PrimitiveDateTime};
 
 const DELPHI_EPOCH: PrimitiveDateTime = datetime!(1899-12-30 0:00);
-const MILLISECONDS_OF_SINGLE_DAY: i128 = Duration::days(1).whole_milliseconds();
+const NANOSECONDS_OF_SINGLE_DAY: i128 = Duration::days(1).whole_nanoseconds();
 
 #[derive(Copy, Clone, Debug)]
 pub struct DelphiTimestamp(pub(crate) f64);
@@ -29,7 +29,7 @@ impl From<PrimitiveDateTime> for DelphiTimestamp {
     fn from(pdt: PrimitiveDateTime) -> Self {
         let value = pdt - DELPHI_EPOCH;
         let days = value.whole_days();
-        let fraction = (value - Duration::days(days)).whole_milliseconds() as f64 / MILLISECONDS_OF_SINGLE_DAY as f64;
+        let fraction = (value - Duration::days(days)).whole_nanoseconds() as f64 / NANOSECONDS_OF_SINGLE_DAY as f64;
         let timestamp = days as f64 + fraction;
         Self(timestamp)
     }
@@ -38,8 +38,8 @@ impl From<PrimitiveDateTime> for DelphiTimestamp {
 impl From<DelphiTimestamp> for PrimitiveDateTime {
     fn from(timestamp: DelphiTimestamp) -> Self {
         let pdt = DELPHI_EPOCH + Duration::days(timestamp.0.trunc() as i64);
-        let pdt = pdt + Duration::milliseconds(
-            (timestamp.0.fract() * MILLISECONDS_OF_SINGLE_DAY as f64) as i64
+        let pdt = pdt + Duration::nanoseconds(
+            (timestamp.0.fract() * NANOSECONDS_OF_SINGLE_DAY as f64) as i64
         );
         pdt
     }
@@ -52,11 +52,11 @@ mod tests {
 
     #[test]
     fn test_primitive_date_time_to_delphi_timestamp() {
-        assert_eq!(DelphiTimestamp::from(datetime!(2000-12-30 12:30)), DelphiTimestamp::from(36890.520833333336));
+        assert_eq!(DelphiTimestamp::from(datetime!(2000-12-30 12:30:00.000000209)), DelphiTimestamp::from(36890.520833333336));
     }
 
     #[test]
     fn test_delphi_timestamp_to_primitive_date_time() {
-        assert_eq!(PrimitiveDateTime::from(DelphiTimestamp::from(36890.520833333336)), datetime!(2000-12-30 12:30:00));
+        assert_eq!(PrimitiveDateTime::from(DelphiTimestamp::from(36890.520833333336)), datetime!(2000-12-30 12:30:00.000000209));
     }
 }
