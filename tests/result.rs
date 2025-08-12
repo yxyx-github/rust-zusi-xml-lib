5e238ffab58860b265051b909b353aa4cd9ad9c3
yxyx-github <dev_mw@vodafonemail.de>
2025-08-12 10:33:32 +0200

    integration tests: reuse TestRunner instances

--- tests/result.rs
+++ tests/result.rs
@@ -1,28 +1,37 @@
 mod deserialized;
 mod utils;
 
+use zusi_xml_lib::xml::zusi::Zusi;
 use crate::deserialized::result;
 use crate::utils::TestRunner;
 
 const XML_PATH: &str = "./tests/serialized/result.xml";
 const XML_PATH_WITHOUT_OPTIONALS: &str = "./tests/serialized/result-without-optionals.xml";
 
+fn runner() -> TestRunner<Zusi> {
+    TestRunner::new_with_file(XML_PATH, result::deserialized(true))
+}
+
+fn runner_without_optionals() -> TestRunner<Zusi> {
+    TestRunner::new_with_file(XML_PATH_WITHOUT_OPTIONALS, result::deserialized(false))
+}
+
 #[test]
 fn test_serialize() {
-    TestRunner::new_with_file(XML_PATH, result::deserialized(true)).test_serialize();
+    runner().test_serialize();
 }
 
 #[test]
 fn test_serialize_optionals() {
-    TestRunner::new_with_file(XML_PATH_WITHOUT_OPTIONALS, result::deserialized(false)).test_serialize();
+    runner_without_optionals().test_serialize();
 }
 
 #[test]
 fn test_deserialize() {
-    TestRunner::new_with_file(XML_PATH, result::deserialized(true)).test_serialize();
+    runner().test_serialize();
 }
 
 #[test]
 fn test_deserialize_optionals() {
-    TestRunner::new_with_file(XML_PATH_WITHOUT_OPTIONALS, result::deserialized(false)).test_serialize();
+    runner_without_optionals().test_serialize();
 }
